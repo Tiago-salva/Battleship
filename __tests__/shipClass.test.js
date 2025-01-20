8a7f5d1a244b4c02e97fd91422a9198a5a293edd
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-01-20 20:31:22 -0300

    refactor: remove coordinate dependency from Ship initialization

--- __tests__/shipClass.test.js
+++ __tests__/shipClass.test.js
@@ -1,12 +1,12 @@
 import Ship from "../src/shipClass.js";
 
-const firstShip = new Ship(3, 0, false, [2, 6]);
+const firstShip = new Ship(3);
 
 test("Is the ship sunk?", () => {
-  expect(firstShip.isSunk()).toBe(false);
+  expect(firstShip.isSunk()).toBeFalsy();
 });
 
 test("The ship received a hit", () => {
   firstShip.hit();
-  expect(firstShip.numberOfHits).toBe(2);
+  expect(firstShip.numberOfHits).toBe(1);
 });
