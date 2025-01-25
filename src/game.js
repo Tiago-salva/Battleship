51866fb4fe967c26e1720612c963251c50b7d472
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-01-24 21:01:09 -0300

    feat: remove while loop, now the game flow depends on the events of the player

--- src/game.js
+++ src/game.js
@@ -1,51 +1,58 @@
-import Player from "./playerClass";
+import Player from "./playerClass.js";
+import renderBoard from "./domManager.js";
 
-export default function battleShipGame() {
-  function randomCoordenates(opponent) {
-    const randomRow = Math.floor(
-      Math.random() * opponent.playerGameboard.gameboard.length
-    );
-    const randomCol = Math.floor(
-      Math.random() * opponent.playerGameboard.gameboard[0].length
-    );
+const gameTurnText = document.querySelector(".game-turn");
+let winner = null;
+let gameTurn = humanPlayer;
 
-    return [randomRow, randomCol];
+function checkWinner(currentPlayer, opponent) {
+  if (opponent.playerGameboard.allShipsSunk() === true) {
+    winner = currentPlayer.name;
+    return true;
   }
 
-  function checkWinner(currentPlayer, opponent) {
-    if (opponent.playerGameboard.allShipsSunk() === true) {
-      winner = currentPlayer.name;
-      return true;
-    }
+  return false;
+}
 
-    return false;
+export default function handleHumanClick(cellDiv, opponent) {
+  const coordinates = cellDiv.dataset.coordinates.split(",").map(Number);
+  gameTurnText.textContent = gameTurn;
+  humanPlayer.attack(opponent, coordinates);
+  renderBoard(opponent);
+
+  if (checkWinner(humanPlayer, opponent)) {
+    console.log(`Ganaste: ${winner.name}`);
+    return;
   }
 
+  gameTurn = computerPlayer;
+  setTimeout(() => {
+    computerTurn();
+  }, 1000);
+}
+
+function computerTurn() {
+  gameTurnText.textContent = gameTurn;
+  computerPlayer.attack(humanPlayer);
+  renderBoard(humanPlayer);
+
+  if (checkWinner(computerPlayer, humanPlayer)) {
+    console.log(`You win: ${winner.name}`);
+    return;
+  }
+
+  gameTurn = humanPlayer;
+}
+
+function battleShipGame() {
   const humanPlayer = new Player("Valentin", "human");
   const computerPlayer = new Player("Gustavo", "computer");
-  let winner = null;
-  let gameTurn = humanPlayer;
 
   humanPlayer.playerGameboard.placeShip(2, [2, 6]);
   computerPlayer.playerGameboard.placeShip(2, [5, 1], false);
 
-  // Mas tarde agregar que cuando alguien le de a un barco, no cambie el turno
-  while (winner === null) {
-    if (gameTurn === humanPlayer) {
-      const playerCoordenates = randomCoordenates(computerPlayer);
-      humanPlayer.attack(computerPlayer, playerCoordenates);
-      checkWinner(humanPlayer, computerPlayer);
-      gameTurn = computerPlayer;
-    } else {
-      computerPlayer.attack(humanPlayer);
-      checkWinner(computerPlayer, humanPlayer);
-      gameTurn = humanPlayer;
-    }
-  }
-
-  return {
-    winner,
-    humanPlayerBoard: humanPlayer.playerGameboard.gameboard,
-    computerPlayerBoard: computerPlayer.playerGameboard.gameboard,
-  };
+  renderBoard(humanPlayer);
+  renderBoard(computerPlayer);
 }
+
+battleShipGame();
