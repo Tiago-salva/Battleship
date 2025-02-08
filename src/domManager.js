0a050c538e92d8c6b43a97c19c1b2cb0a0755122
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-02-08 18:10:29 -0300

    refactor: move some lines with no importance

--- src/domManager.js
+++ src/domManager.js
@@ -14,15 +14,18 @@ export default function renderBoard(player, handleClick = null) {
 
       if (player.type === "computer") {
         cellDiv.classList.add("cell-computer");
+        cellDiv.addEventListener("click", () => handleClick(cellDiv, player));
       } else {
         cellDiv.classList.add("cell-human");
+        cellDiv.addEventListener("click", () => handleClick(cellDiv, player));
       }
-      // if (
-      //   player.type === "human" &&
-      //   player.playerGameboard.gameboard[rowIndex][colIndex] === 1
-      // ) {
-      //   cellDiv.style.border = "1px solid red";
-      // }
+
+      if (
+        player.type === "human" &&
+        player.playerGameboard.gameboard[rowIndex][colIndex] === 1
+      ) {
+        cellDiv.style.border = "1px solid red";
+      }
 
       if (player.playerGameboard.gameboard[rowIndex][colIndex] === "O") {
         cellDiv.classList.add("cell-empty");
@@ -40,8 +43,6 @@ export default function renderBoard(player, handleClick = null) {
       cellDiv.dataset.index = rowIndex * 10 + colIndex;
       cellDiv.dataset.coordinates = `${rowIndex},${colIndex}`;
       gameboardContainer.appendChild(cellDiv);
-      if (player.type === "computer")
-        cellDiv.addEventListener("click", () => handleClick(cellDiv, player));
     });
   });
 }
